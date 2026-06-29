d54ac75c0fc51cdf29cac60c86bda5b29ea813be
Manas-2007 <patidarmanas01@gmail.com>
2026-06-29 16:57:18 +0530

    git ignore

--- Myntra/app/_layout.tsx
+++ Myntra/app/_layout.tsx
@@ -1,12 +1,21 @@
-import { Stack } from "expo-router";
 import React from "react";
+
 import "../global.css";
+
 import { GlobalProvider } from "./context/GlobalContext";
+import { Stack } from "expo-router";
+
+
 
 export default function Layout() {
+
   return (
+
     <GlobalProvider>
+
        <Stack screenOptions={{ headerShown: false }} />
+
     </GlobalProvider>
+
   );
 }
