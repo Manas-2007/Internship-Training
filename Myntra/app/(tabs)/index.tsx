90ca6c77a7768fee1c1acc01914407df180d0324
Manas-2007 <patidarmanas01@gmail.com>
2026-06-29 17:04:55 +0530

    fixed asset issue

--- Myntra/app/(tabs)/index.tsx
+++ Myntra/app/(tabs)/index.tsx
@@ -128,11 +128,11 @@ export default function Home() {
       {/* 1. Sleek Header Section */}
       <View className="flex-row justify-between items-center px-4 py-1 bg-white border-b border-neutral-100">
         <View className="flex-row items-center gap-2">
-          <Image
-            source={require("../../assets/images/favicon.png")}
-            style={{ width: 28, height: 28 }}
-            resizeMode="contain"
-          />
+    <Image
+  source={require("../../assets/images/myntra.jpg")} 
+  style={{ width: 28, height: 28 }}
+  resizeMode="contain"
+/>
           <Text className="text-xl font-black text-neutral-900 tracking-wider">
             MYNTRA
           </Text>
